03607f5532cc8cd68f032da769042b5da20058b4
Andika Afif <andika.afif@idstar.co.id>
2026-01-09 22:45:21 +0700

    update

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -26,7 +26,7 @@ android {
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         // minSdk = flutter.minSdkVersion
         // targetSdk = flutter.targetSdkVersion
-        minSdk = 24
+        minSdk = 25
         targetSdk = flutter.targetSdkVersion
         versionCode = flutter.versionCode
         versionName = flutter.versionName
